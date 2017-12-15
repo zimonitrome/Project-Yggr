c6c1bf17ebb3cbc3845fdbfc747339d441a7f466
Filip Andersson <jabos123@gmail.com>
2017-12-15 19:58:56 +0100

    First commit on empty Android project this is template for empty and i have named it option activity

---
+++ app/src/main/java/com/example/jabos/project_yggr/OptionActivity.java
@@ -0,0 +1,13 @@
+package com.example.jabos.project_yggr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+    }
+}
